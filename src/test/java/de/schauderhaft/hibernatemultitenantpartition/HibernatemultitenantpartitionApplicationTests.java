91eff7d8b45037e37fca85774646b5de789d98c2
Jens Schauder <jschauder@vmware.com>
2022-07-15 10:59:55 +0200

    Simple test writing and loading data.
    No multitenancy yet.

--- src/test/java/de/schauderhaft/hibernatemultitenantpartition/HibernatemultitenantpartitionApplicationTests.java
+++ src/test/java/de/schauderhaft/hibernatemultitenantpartition/HibernatemultitenantpartitionApplicationTests.java
@@ -1,13 +1,42 @@
 package de.schauderhaft.hibernatemultitenantpartition;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class HibernatemultitenantpartitionApplicationTests {
 
+	@Autowired
+	Persons persons;
+
+	@Autowired
+	TransactionTemplate txTemplate;
+
 	@Test
-	void contextLoads() {
+	void saveAndLoadPerson() {
+
+		Person adam = txTemplate.execute(tx ->
+				{
+					Person person = Persons.named("Adam");
+					return persons.save(person);
+				}
+		);
+
+		assertThat(adam.getId()).isNotNull();
+
+		Person reloaded = txTemplate.execute(
+				tx -> persons.findById(adam.getId()).orElseThrow()
+		);
+
+		// makes sure we aren't fooled by JPAs cache
+		assertThat(reloaded).isNotSameAs(adam);
+
 	}
 
 }
