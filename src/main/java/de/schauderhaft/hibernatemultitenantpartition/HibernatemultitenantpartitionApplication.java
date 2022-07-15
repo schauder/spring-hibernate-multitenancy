8bcecfbb2db4f623e7c89cbb0e44c48e015de0b2
Jens Schauder <jschauder@vmware.com>
2022-07-15 10:42:52 +0200

    Initial Commit.

---
+++ src/main/java/de/schauderhaft/hibernatemultitenantpartition/HibernatemultitenantpartitionApplication.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.hibernatemultitenantpartition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatemultitenantpartitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatemultitenantpartitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/schauderhaft/hibernatemultitenantpartition/HibernatemultitenantpartitionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.hibernatemultitenantpartition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatemultitenantpartitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
