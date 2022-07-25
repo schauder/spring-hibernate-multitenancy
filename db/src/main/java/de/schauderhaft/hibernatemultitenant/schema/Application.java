a9b6d9207ddccd98613a6ffca47d2d8e2a0a84e3
Jens Schauder <jschauder@vmware.com>
2022-07-25 13:21:40 +0200

    create database example.

---
+++ db/src/main/java/de/schauderhaft/hibernatemultitenant/schema/Application.java
@@ -0,0 +1,55 @@
+package de.schauderhaft.hibernatemultitenant.schema;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
+import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.lookup.BeanFactoryDataSourceLookup;
+import org.springframework.orm.hibernate5.SpringBeanContainer;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+			CurrentTenantIdentifierResolver currentTenantIdentifierResolver,
+			MultiTenantConnectionProvider connectionProvider, ConfigurableListableBeanFactory beanFactory
+
+	) {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+
+		em.setPackagesToScan("de.schauderhaft.hibernatemultitenant.schema");
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+
+		Map<String, Object> properties = new HashMap<>();
+		properties.put(AvailableSettings.MULTI_TENANT_IDENTIFIER_RESOLVER, currentTenantIdentifierResolver);
+		properties.put(AvailableSettings.MULTI_TENANT_CONNECTION_PROVIDER, connectionProvider);
+		properties.put(AvailableSettings.BEAN_CONTAINER, new SpringBeanContainer(beanFactory));
+		properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.H2Dialect");
+		em.setJpaPropertyMap(properties);
+
+		return em;
+	}
+
+
+}
---
+++ db/src/test/java/de/schauderhaft/hibernatemultitenant/schema/ApplicationTests.java
@@ -0,0 +1,60 @@
+package de.schauderhaft.hibernatemultitenant.schema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@TestExecutionListeners(listeners = {DependencyInjectionTestExecutionListener.class})
+class ApplicationTests {
+
+	public static final String PIVOTAL = "PIVOTAL";
+	public static final String VMWARE = "VMWARE";
+	@Autowired
+	Persons persons;
+
+	@Autowired
+	TransactionTemplate txTemplate;
+
+	@Autowired
+	TenantIdentifierResolver currentTenant;
+
+	@Test
+	void saveAndLoadPerson() {
+
+		Person adam = createPerson(PIVOTAL, "Adam");
+		Person eve = createPerson(VMWARE, "Eve");
+
+
+		currentTenant.setCurrentTenant(VMWARE);
+		assertThat(persons.findAll()).extracting(Person::getName).containsExactly("Eve");
+
+
+		currentTenant.setCurrentTenant(PIVOTAL);
+		assertThat(persons.findAll()).extracting(Person::getName).containsExactly("Adam");
+
+
+
+	}
+
+	private Person createPerson(String schema, String name) {
+
+		currentTenant.setCurrentTenant(schema);
+
+		Person adam = txTemplate.execute(tx ->
+				{
+					Person person = Persons.named(name);
+					return persons.save(person);
+				}
+		);
+
+		assertThat(adam.getId()).isNotNull();
+		return adam;
+	}
+}
